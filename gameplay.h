7ec700369f7df3fb5badcac7468e72fe18e4b4fc
Ung Sylvain <sylvain.ung@etu.u-pec.fr>
2015-04-19 23:45:29 +0200

    Essai avec 1 tube nommé (warning unused var)

--- gameplay.h
+++ gameplay.h
@@ -1,15 +1,18 @@
-// #ifndef _GAMEPLAY_INCLUDED
-// #define _GAMEPLAY_INCLUDED
+#ifndef _GAMEPLAY_INCLUDED
+#define _GAMEPLAY_INCLUDED
 
-// #include <unistd.h>
-// #include <fcntl.h>
-// #include <sys/stat.h>
-// #include <mqueue.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <time.h>
 
-// #include "term.h"
-// #include "player.h"
-// #include "mylib.h"
+#include "term.h"
+#include "player.h"
+#include "mylib.h"
 
-// void initGameplay();
+void initGameplay();
 
-// #endif
+char *random_named_pipe();
+
+#endif
